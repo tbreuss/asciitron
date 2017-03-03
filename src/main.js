c36762c59a1056504e5aef551efd97c6a2c40ec2
Thomas Breuss <thomasbreuss@gmx.ch>
2017-03-03 11:48:11 +0100

    Fixed wrong var name

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ function createWindow() {
     // Open the DevTools.
     //mainWindow.webContents.openDevTools()
     mainWindow.webContents.on("devtools-opened", () => {
-        win.webContents.closeDevTools();
+        mainWindow.webContents.closeDevTools();
     });
 
     // Emitted when the window is closed.
