ed2b91aa28eb15985573c20a713eb973452adf47
tbreuss <thomasbreuss@gmx.ch>
2020-10-22 20:00:21 +0200

    Fix auto-updater

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const {app, BrowserWindow, Menu, ipcMain, autoUpdater} = require('electron')
+const {app, BrowserWindow, Menu, ipcMain } = require('electron')
+const {autoUpdater} = require('electron-updater')
 
 const store = require('./store')
 const path = require('path')
@@ -126,7 +127,7 @@ function createMainWindow() {
 
 app.on('ready', () => {
     createMainWindow()
-    autoUpdater.checkForUpdatesAndNotify();
+    autoUpdater.checkForUpdatesAndNotify()
 })
 
 app.on('window-all-closed', () => {
